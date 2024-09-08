68db90dfcbb6bb1db5d170be14985e4f7e71d525
DEO579 <deo.kr59998@gmail.com>
2024-09-08 09:04:55 +0530

    AVNGER -4 build spring boot data rest

---
+++ spring-data-rest/src/main/java/com/company/spring_data_rest_api/spring_data_rest/SpringDataRestApplication.java
@@ -0,0 +1,13 @@
+package com.company.spring_data_rest_api.spring_data_rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataRestApplication.class, args);
+	}
+
+}
---
+++ spring-data-rest/src/test/java/com/company/spring_data_rest_api/spring_data_rest/SpringDataRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.spring_data_rest_api.spring_data_rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
